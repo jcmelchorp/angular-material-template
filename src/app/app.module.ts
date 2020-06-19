a8039923b6d73ea3347d2c0eb0663ed04b9a7c2a
Julio César Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-06-19 10:39:54 -0500

    create angular material module for imports/exports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AppMaterialModule } from './app-material/app-material.module';
+import { AppMaterialModule } from './app-material.module';
 
 @NgModule({
   declarations: [
