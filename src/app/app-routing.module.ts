ecfe6859cb415e388e96ef3b2b0d6e43d52bf2d2
Julio César Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-06-19 22:13:40 -0500

    add wellcome view and app brand

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WellcomeComponent } from './core/wellcome/wellcome.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: WellcomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { AppMaterialModule } from './app-material.module';
 
 import { AppComponent } from './app.component';
+import { CoreModule, FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { ContentComponent } from './core/content/content.component';
-
-
+import { AppMaterialModule } from './app-material.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, FooterComponent, ContentComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    AppMaterialModule,
     FontAwesomeModule,
+    AppMaterialModule,
     FlexLayoutModule,
+    CoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
