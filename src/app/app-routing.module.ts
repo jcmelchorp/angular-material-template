b091c37eec58c401841e5b3dbc107b55218c0f34
Julio César Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-06-20 13:29:57 -0500

    footer complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,6 @@ import { WellcomeComponent } from './core/wellcome/wellcome.component';
 const routes: Routes = [
   { path: '', component: WellcomeComponent },
   {
-    path: 'module-navigator',
-    loadChildren: () =>
-      import('./modules/module-navigator/module-navigator.module').then(
-        (m) => m.ModuleNavigatorModule
-      ),
-  },
-  {
     path: 'module-two',
     loadChildren: () =>
       import('./modules/module-two/module-two.module').then(
